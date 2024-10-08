9f01e53cfb0be62bfb9dccc05e0fd65fa590478a
Junshen18 <junshenhere@gmail.com>
2024-10-08 22:12:42 +0800

    update homepage link

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -6,14 +6,14 @@ export function middleware(request: NextRequest) {
 
   if (!authToken) {
     if (request.nextUrl.pathname === '/') {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL('/welcome', request.url))
     }
     if (['/votinglists', '/createvote'].includes(request.nextUrl.pathname)) {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL('/welcome', request.url))
     }
   }
 
-  if (authToken && request.nextUrl.pathname === '/login') {
+  if (authToken && request.nextUrl.pathname === '/welcome') {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
